7b63a2794d64c5f8e87a0c0cc16aeb863a0c4db4
Umut YILDIRIM <umut475@gmail.com>
2024-03-24 04:49:21 -0700

    Add new files and update paths

--- ViZDoom/examples/python/llm_game.py
+++ ViZDoom/examples/python/llm_game.py
@@ -1,24 +1,27 @@
 from vizdoom import DoomGame, Mode
-#from vizdoom.opencv import ScreenResolutionManager
-#import cv2
+# from vizdoom.opencv import ScreenResolutionManager
+# import cv2
 import os
 from pynput import keyboard
 import time
 import numpy as np
 import json
+import openai
 
 game = DoomGame()
-#game.load_config("basic.cfg")
-game.load_config(os.path.join('/Users/bhav/experiments/mistral-hackathon/repos/GameCopilot/ViZDoom/scenarios', "basic.cfg"))
+# game.load_config("basic.cfg")
+game.load_config(os.path.join(
+    '/Users/hope/Desktop/Projects/GameCopilot/ViZDoom/scenarios', "basic.cfg"))
 game.set_window_visible(True)
 game.set_mode(Mode.ASYNC_PLAYER)
 game.set_labels_buffer_enabled(True)
 game.set_render_hud(False)
 game.init()
 
-#resolution_manager = ScreenResolutionManager()
-#resolution_manager.set_game(game)
-#resolution_manager.set_mode(Mode.ASYNC_PLAYER)
+# resolution_manager = ScreenResolutionManager()
+# resolution_manager.set_game(game)
+# resolution_manager.set_mode(Mode.ASYNC_PLAYER)
+
 
 def generate_ascii_grid(bounding_boxes, wall_buffer, floor_buffer, screen_width, screen_height, grid_width=64, grid_height=32):
     # Normalize screen dimensions to 32x32 grid
@@ -84,23 +87,29 @@ def get_object_name_char(object_name):
     else:
         return object_name[0]
 
+
 def convert_labels_to_representation(labels, wall_buffer, floor_buffer, screen_height=320, screen_width=240):
     reps = []
-    #screen_height = 320
-    #screen_width = 240
+    # screen_height = 320
+    # screen_width = 240
     for label in labels:
-        #rep = (32*label.x/screen_width, 32*label.y/screen_height, 32*label.width/screen_width, 32*label.height/screen_height, label.object_name[0])
-        rep = (label.x, label.y, label.width, label.height, get_object_name_char(label.object_name))
-        #rep = (label.y, label.x, label.height, label.width, label.object_name[0])
+        # rep = (32*label.x/screen_width, 32*label.y/screen_height, 32*label.width/screen_width, 32*label.height/screen_height, label.object_name[0])
+        rep = (label.x, label.y, label.width, label.height,
+               get_object_name_char(label.object_name))
+        # rep = (label.y, label.x, label.height, label.width, label.object_name[0])
         print(label.object_name)
         reps.append(rep)
-    grid = generate_ascii_grid(reps, wall_buffer, floor_buffer, screen_height, screen_width)
+    grid = generate_ascii_grid(
+        reps, wall_buffer, floor_buffer, screen_height, screen_width)
     return grid
+
 
 def one_hot(i, max_num=7):
     arr = [False for _ in range(max_num)]
     arr[i] = True
     return arr
+
+
 '''
 key_mappings = {
     keyboard.Key.left: Button.TURN_LEFT,
@@ -113,13 +122,13 @@ key_mappings = {
 }
 '''
 available_actions = [
-                'MOVE_FORWARD',
-                'TURN_LEFT',
-                'TURN_RIGHT',
-                'MOVE_BACKWARD',
-                'MOVE_LEFT',
-                'MOVE_RIGHT',
-                'ATTACK'
+    'MOVE_FORWARD',
+    'TURN_LEFT',
+    'TURN_RIGHT',
+    'MOVE_BACKWARD',
+    'MOVE_LEFT',
+    'MOVE_RIGHT',
+    'ATTACK'
 ]
 key_mappings = {
     keyboard.Key.up: 0,
@@ -136,28 +145,41 @@ action_mappings = {
     0: one_hot(0),   # Move forward
     1: one_hot(1),   # Turn left
     2: one_hot(2),    # Turn right
-    3: one_hot(3),    # Move backward 
-    4: one_hot(4),    # Move left 
+    3: one_hot(3),    # Move backward
+    4: one_hot(4),    # Move left
     5: one_hot(5),    # Move right
-    6: one_hot(6),    # Attack 
+    6: one_hot(6),    # Attack
     7: None,
 }
 
 pressed_keys = set()
 
-#def on_press(key):
+# def on_press(key):
 #    if key in key_mappings:
 #        pressed_keys.add(key_mappings[key])
 
-#def on_release(key):
+# def on_release(key):
 #    if key in key_mappings:
 #        pressed_keys.discard(key_mappings[key])
 
-#listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-#listener.start()
+# listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+# listener.start()
 
-def llm_call(last_state):
-    return None
+
+def llm_call(grid):
+    client = openai.OpenAI(
+        base_url="https://api.fireworks.ai/inference/v1",
+        api_key="62KUj5M5I2rNBSUE0MFi53VT8WOBsFLnx3QCr2zYeY23iCdf",
+    )
+    response = client.completions.create(
+        model="accounts/umut475-2d9d48/models/doom-mistral",
+        temperature=0.1,
+        max_tokens=4096,
+        prompt="You're the DOOM AI, assuming the role of 'Demon Slayer' in a grid environment represented by ASCII characters. Understand each character as follows: E: Enemy, P: Player, B: Bullet, W: Wall, F: Floor, A: Armor Bonus, Z: Zombieman, H: Health Bonus, S: Stimpack. Your task is to interpret the grid and choose an appropriate action from the following options: MOVE_FORWARD, TURN_LEFT, TURN_RIGHT, MOVE_BACKWARD, MOVE_LEFT, MOVE_RIGHT, ATTACK. For this instance, your chosen action should be conveyed simply and directly as: `MOVE_FORWARD`. Your responses must exclusively be your chosen action. "+grid,
+    )
+    print(response.choices[0].text)
+    return response.choices[0].text
+
 
 episodes = 10
 screen_height = 320
@@ -174,21 +196,21 @@ for episode in range(episodes):
         state = game.get_state()
 
         # Get user input for action
-        #action_input = input("Enter action (0-3): ")
-        #if len(pressed_keys) == 0:
+        # action_input = input("Enter action (0-3): ")
+        # if len(pressed_keys) == 0:
         #    continue
-        #pressed_key = list(pressed_keys)[0]
-        #print('Pressed key: ', pressed_key)
+        # pressed_key = list(pressed_keys)[0]
+        # print('Pressed key: ', pressed_key)
         try:
-            #action_index = pressed_key#int(key_mappings[pressed_key])
-            #if action_index == 7:
+            # action_index = pressed_key#int(key_mappings[pressed_key])
+            # if action_index == 7:
             #    # End game
             #    break
             if next_action is None:
                 action = action_mappings[0]
             else:
                 action = next_action
-            #action = action_mappings.get(action_index, [False, False, False])
+            # action = action_mappings.get(action_index, [False, False, False])
 
             wall_buffer = np.zeros_like(state.labels_buffer)
             floor_buffer = np.zeros_like(state.labels_buffer)
@@ -198,7 +220,8 @@ for episode in range(episodes):
             print(state.labels)
             for label in state.labels:
                 all_labels[label.object_name] = 0
-            grid = convert_labels_to_representation(state.labels, wall_buffer, floor_buffer, screen_height=320, screen_width=240)
+            grid = convert_labels_to_representation(
+                state.labels, wall_buffer, floor_buffer, screen_height=320, screen_width=240)
             print(grid)
             example = {}
             example['grid_height'] = 32
@@ -216,17 +239,17 @@ for episode in range(episodes):
             example['reward'] = reward
             episode_data.append(example)
             # TODO: Make api call to LLM.
-            next_action = llm_call(episode_data[-1])
+            next_action = llm_call(grid)
 
         except ValueError:
             print("Invalid input. Using random action.")
-            #action = choice([0, 1, 2, 3])
+            # action = choice([0, 1, 2, 3])
 
         print(f"State: {state.number}, Action: {action}, Reward: {reward}")
 
         # Capture and save the frame
-        #frame = resolution_manager.grab_screen()
-        #cv2.imwrite(f"frames/frame_{state.number}.png", frame)
+        # frame = resolution_manager.grab_screen()
+        # cv2.imwrite(f"frames/frame_{state.number}.png", frame)
 
         fp = open(f'./training_data/episode_{episode}.json', 'w')
         json.dump(episode_data, fp, indent=4)
--- main.py
+++ main.py
@@ -1,28 +1,91 @@
-# GameCopilot
-# Todo: Connect GROG with RAG
-# Connect MongoDB with RAG
-
-import embedding
+from huggingface_hub import InferenceClient
+import gradio as gr
 from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
-from decouple import config
+from pymongo import MongoClient
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+from langchain_mistralai import MistralAIEmbeddings
 
-GROG_API_KEY = config('GROG_API_KEY')
-
-# MistralAI Embedding
-embedding.embedQuery("Tell me a joke.")
-
-
-# GROG API KEY
-chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768",
-                api_key=GROG_API_KEY)
+client = ChatGroq(
+    api_key="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")
 
 
-system = "You are a helpful assistant."
-human = "{text}"
-prompt = ChatPromptTemplate.from_messages(
-    [("system", system), ("human", human)])
+mongo_client = MongoClient(
+    "mongodb+srv://umut475:wiiAcSrI6OFdeLog@gamecopilotrag.20hgbqx.mongodb.net/?retryWrites=true&w=majority&appName=GameCopilotRAG")
+db = "gamecopilot"
+vector_search_collection = "all"
 
-chain = prompt | chat
-for chunk in chain.stream({"text": "Tell me a joke."}):
-    print(chunk.content, end="", flush=True)
+vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+    "mongodb+srv://umut475:wiiAcSrI6OFdeLog@gamecopilotrag.20hgbqx.mongodb.net/?retryWrites=true&w=majority&appName=GameCopilotRAG",
+    db + "." + vector_search_collection,
+    MistralAIEmbeddings(
+        mistral_api_key="TWfVrlX659GSTS9hcsgUcPZ8uNzfoQsg", model="mistral-embed"),
+)
+
+
+def format_prompt(message, history):
+    prompt = "<s>"
+    for user_prompt, bot_response in history:
+        prompt += f"[INST] {user_prompt} [/INST]"
+        prompt += f" {bot_response}</s> "
+    prompt += f"[INST] {message} [/INST]"
+    return prompt
+
+
+def generate(prompt, history, temperature=0.9, max_new_tokens=256):
+    temperature = float(temperature)
+    if temperature < 1e-2:
+        temperature = 1e-2
+
+    generate_kwargs = dict(
+        temperature=temperature,
+        max_tokens=max_new_tokens,
+    )
+
+    results = vector_search.similarity_search(prompt)
+
+    # Display results
+    for result in results:
+        print("Results: ", result)
+
+    formatted_prompt = format_prompt(prompt, history)
+
+    stream = client.stream(
+        formatted_prompt, **generate_kwargs)
+    output = ""
+
+    for chunk in stream:
+        output += chunk.content
+        yield output
+    return output
+
+
+additional_inputs = [
+    gr.Slider(
+        label="Temperature",
+        value=0.9,
+        minimum=0.0,
+        maximum=1.0,
+        step=0.05,
+        interactive=True,
+        info="Higher values produce more diverse outputs",
+    ),
+    gr.Slider(
+        label="Max new tokens",
+        value=256,
+        minimum=0,
+        maximum=1048,
+        step=64,
+        interactive=True,
+        info="The maximum numbers of new tokens",
+    )
+]
+
+
+gr.ChatInterface(
+    fn=generate,
+    chatbot=gr.Chatbot(show_label=False, show_share_button=False,
+                       show_copy_button=True, likeable=True, layout="panel"),
+    additional_inputs=additional_inputs,
+    title="""GameCopilot""",
+    description="""GameCopilot is a chatbot that helps you to find the best games for you. Powered by Groq, MistralAI(Embeddings) and MongoDB Atlas.""",
+).launch(show_api=False)
